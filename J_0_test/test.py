ad66a4f4a19672a7411e131e5825d42ed9d13e01
lesgreed <157906211+lesgreed@users.noreply.github.com>
2024-09-27 16:05:04 +0200

    27

--- J_0_test/test.py
+++ J_0_test/test.py
@@ -5,9 +5,12 @@ os.chdir('J_0_test')
 print("Current working directory:", os.getcwd())
             
     
-mconf_config = {'B0': 2.525, #B field at magentic axis at toroidal angle B0_angle
-                  'B0_angle': 0.0,
-                  'extraLCMS': 1.8,   #Flux surfaces extrapolation parameter (s_max)
-                  'accuracy': 1e-10, #accuracy of magnetic to cartesian coordinat transformation
-                  'truncation': 1e-10}  #truncation when generating grid 
-eq0 = mconf.Mconf_equilibrium('w7x-sc1.bc', mconf_config)
+mconf_config = {'B0': 2.525,
+                'B0_angle': 0.0,
+                'accuracy': 1e-10, #accuracy of magnetic to cartesian coordinat transformation
+                'truncation': 1e-10} #trancation of mn harmonics
+eq = mconf.Mconf_equilibrium('w7x-sc1.bc',mconf_config=mconf_config)
+ 
+# Getting B-field
+tmp = eq.get_s_and_B(5.9,0.,0)
+print(tmp)
